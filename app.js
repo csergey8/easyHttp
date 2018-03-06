e242b7953c58d9c7f96e0a97549eb95e9435301b
csergey8 <c.sergey@protonmail.com>
2018-03-06 17:22:57 +0200

    simple http library ended

--- app.js
+++ app.js
@@ -1,10 +1,58 @@
 const http = new easyHTTP();
 
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts){
+
+// GET Posts
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts){
+//     if(err) {
+//         alert(err);
+//     } else {
+//         console.log(posts);
+//     }
+// });
+
+
+
+
+// Get a post 
+
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function (err, post) {
+//     if (err) {
+//         alert(err);
+//     } else {
+//         console.log(post);
+//     }
+// });
+
+// POST 
+const data = {
+    title: 'Custom post',
+    body: 'This is custom post'
+};
+
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post){
+//     if(err) {
+//         alert(err);
+//     } else {
+//         console.log(post);
+//     }
+// })
+
+
+// PUT
+
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post){
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//     }
+// });
+
+// DELETE
+  http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response){
     if(err) {
         alert(err);
     } else {
-        console.log(posts);
+        console.log(response);
     }
 });
-
